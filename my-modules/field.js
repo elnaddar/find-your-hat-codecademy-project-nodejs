bddc534aa8cbd8e5272b4e7433f86af7d901cc37
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-21 00:25:25 +0200

    added field print method

--- my-modules/field.js
+++ my-modules/field.js
@@ -2,7 +2,12 @@ class Field{
     constructor(field){
         this.field = field;
     }
-
+    
+    print(){
+        this.field.forEach(row => {
+            console.log(row.join(""));
+        });
+    }
 }
 
 module.exports = {Field};
