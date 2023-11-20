39cd1216d20ea98098dc841d271c287087d1dba7
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-21 01:55:41 +0200

    created setLocationTo method to change value in a location

--- my-modules/field.js
+++ my-modules/field.js
@@ -7,6 +7,11 @@ class Field{
         return [this.field.length, this.field[0].length]
     }
 
+    setLocationTo(loc, val){
+        let [x, y] = loc;
+        this.field[x][y] = val;
+    }
+
     print(){
         this.field.forEach(row => {
             console.log(row.join(""));
