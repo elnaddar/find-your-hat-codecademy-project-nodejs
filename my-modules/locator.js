2a90a760dd303cae6ef1f64756279eff7389f5ae
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-21 01:33:46 +0200

    created and tested locator module to help locate character in the game

---
+++ my-modules/locator.js
@@ -0,0 +1,67 @@
+class Locator{
+    constructor(boundries, initPos=[0,0]){
+        this.getInitPos = ()=>initPos;
+        this.getBoundries = ()=>boundries;
+        this.currentLocation = initPos;
+    }
+
+    get xBoundries (){
+        return this.getBoundries()[0];
+    }
+
+    get yBoundries (){
+        return this.getBoundries()[1];
+    }
+
+    get x(){
+        return this.currentLocation[0];
+    }
+
+    get y(){
+        return this.currentLocation[1];
+    }
+    
+    get canGoBottom(){
+        return this.x + 1 < this.xBoundries;
+    }
+
+    get canGoTop(){
+        return this.x - 1 >= 0;
+    }
+    
+    get canGoRight(){
+        return this.y + 1 < this.yBoundries;
+    }
+
+    get canGoLeft(){
+        return this.y - 1 >= 0;
+    }
+
+    goTo(dir){
+        let loc = this.peak(dir);
+        if(loc != null){
+            this.currentLocation = loc;
+        } else{
+            console.log(`You can't go ${dir}`);
+        }
+        console.log(this.currentLocation);
+    }
+
+    peak(dir){
+        let [x, y] = this.currentLocation;
+        switch(dir){
+            case 'd':
+                return this.canGoBottom? [x+1, y] : null;
+            case 't':
+                return this.canGoTop? [x-1, y]: null;
+            case 'r':
+                return this.canGoRight? [x, y+1]: null;
+            case 'l':
+                return this.canGoLeft? [x, y-1]: null;
+        }
+        return null;
+    }
+    
+}
+
+module.exports = {Locator};
