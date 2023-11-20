e5a41a8f9026353fa9ffd281c866da5deff26270
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-21 01:33:06 +0200

    created get boundries method in field to help in game logic

--- my-modules/field.js
+++ my-modules/field.js
@@ -2,7 +2,11 @@ class Field{
     constructor(field){
         this.field = field;
     }
-    
+
+    get boundries(){
+        return [this.field.length, this.field[0].length]
+    }
+
     print(){
         this.field.forEach(row => {
             console.log(row.join(""));
