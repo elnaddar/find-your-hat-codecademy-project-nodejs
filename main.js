36635fcb188bc5a717996f4f1c5c46a50440e02a
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-21 02:20:40 +0200

    finished game interface in main.js

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const prompt = require('prompt-sync')({sigint: true});
-
+const {Field} = require('./my-modules/field');
+const {Game} = require('./my-modules/game');
+const {GameTheme} = require('./my-modules/game-theme');
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+const gameTheme = new GameTheme(hat, hole, fieldCharacter, pathCharacter);
+
+const initField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+  
+const game = new Game(initField, gameTheme);
+game.play();
+
+
+
