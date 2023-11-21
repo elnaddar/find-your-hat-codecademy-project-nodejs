91391fde9eb1fc724e769a72f5166bde4c891c61
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-11-21 02:02:28 +0200

    created game theme class to get game attributes

---
+++ my-modules/game-theme.js
@@ -0,0 +1,7 @@
+class GameTheme{
+    constructor(hat, hole, fieldCharacter, pathCharacter){
+        Object.assign(this, { hat, hole, fieldCharacter, pathCharacter });
+    }
+}
+
+module.exports = {GameTheme};
